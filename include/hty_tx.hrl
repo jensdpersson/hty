c7d20239a48f0744e3bba1881879d90fbcee080b
Jens Persson <jensdpersson@gmail.com>
2015-01-06 20:53:38 +0100

    lots of stuff

---
+++ include/hty_tx.hrl
@@ -0,0 +1,21 @@
+
+
+-record(tx, {
+	  proto="HTTP/1.1",
+	  method='GET',
+	  path={[], []},
+	  status={200, "OK"},
+	  reqh=[],
+	  buffered=[],
+	  ondata=fun(_Data, _State, Htx) -> {ok, noop_state, Htx} end,
+	  ondata_state=q0,
+	  rsph=[],
+	  outs=[],
+	  principal= <<"guest">>,
+	  realm=hty_empty_realm,
+	  attributes=[],
+	  queryparams=[],
+	  socketreader=no,
+	  ndc=[],
+	  log=[],
+	  unread=0}).
